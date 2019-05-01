7595f2f157d76fe4a46f478bf602126a21007ef6
Daviicode <d.ydzdavid@yahoo.com>
2019-05-01 15:49:11 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :dogs
-  resources :employees
+  resources :employees, except: [:destroy]
+  delete '/employees/:id', to:"employees#destroy", as: "delete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
