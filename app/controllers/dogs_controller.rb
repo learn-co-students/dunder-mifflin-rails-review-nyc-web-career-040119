0ab4712d9265c026a0a3abd4bce4187c1b403546
Jacob Lam <jlam18@binghamton.edu>
2019-05-02 09:04:36 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,17 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+
+    def sort
+      @dogs = Dog.all.sort_by{|dog| dog.employees.length}
+      render :index
+    end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all.sort_by{|employee|employee.first_name.downcase }
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      flash[:message] = "Welcome to the club buddy!"
+      redirect_to @employee
+    else
+      flash[:message] = @employee.errors.full_messages[0]
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to @employee
+  end
+
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id, )
+  end
+
 end
